9829160de5e28d7943a0d5c4cbac4c483ad007b6
pn3g20 <pn3g20@soton.ac.uk>
2024-04-08 16:53:11 +0100

    basic ai RB movement with Navmesh pathfinding

--- Assets/Scripts/Player/AIController.cs
+++ Assets/Scripts/Player/AIController.cs
@@ -1,40 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIController : BasePlayerController
 {
-	private Vector3 _target;
+	private Vector3? _target;
 	private Camera _cam;
-
-	// private NavMeshAgent _agent;
+	private NavMeshPath _path;
 
 	protected override void Awake()
 	{
 		base.Awake();
-
-		// _agent = GetComponent<NavMeshAgent>();
-		// _agent.speed = MoveSpeed;
+		
+		_path = new NavMeshPath();
 		_cam = Camera.main;
 	}
 
 	protected override void Update()
 	{
 		base.Update();
-
-		if (Input.GetMouseButtonDown(0) && _cam)
+		
+		if (Input.GetKeyDown(KeyCode.R))
 		{
-			if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out var hit, 100f))
-			{
-				
-			}
-
+			Rb.AddForce(5f * transform.right, ForceMode.Impulse);
 		}
 		
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetMouseButtonDown(0) && _cam)
 		{
-			Debug.Log("pressed space");
+			if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out var hit, 100f, TileMask))
+			{
+				_target = hit.transform.position;
+			}
+		}
+		
+		CheckTarget();
+		
+		if (_target == null) return;
+		
+		NavMesh.CalculatePath(transform.position, (Vector3) _target, NavMesh.AllAreas, _path);
+		
+		DrawPath();
+
+		switch (_path.corners.Length)
+		{
+			case > 1:
+				Move(_path.corners[1]);
+				break;
+			case > 0:
+				Move(_path.corners.Last());
+				break;
+		}
+	}
+
+	private void CheckTarget()
+	{
+		if (_target == null) return;
+		
+		var pos = transform.position;
+		var flatPos = new Vector3(pos.x, 0, pos.z);
+		var diff = (flatPos - (Vector3)_target).magnitude;
+		
+		if (diff > 0.01f) return;
+		
+		_target = null;
+	}
+
+
+	private void Move(Vector3 movePoint)
+	{
+		var direction = (movePoint - transform.position).normalized;		
+		Rb.velocity = Vector3.Lerp(Rb.velocity, MoveSpeed * direction, Time.deltaTime);
+	}
+	
+	private void DrawPath()
+	{
+		for (int i = 0; i < _path.corners.Length - 1; i++)
+		{
+			Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
 		}
 	}
 }
